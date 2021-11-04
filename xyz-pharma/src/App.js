0458f9ec9815128596dfa9357541dc56e9de8bec
Yash Bhole <yash.bhole@ad.infosys.com>
2021-11-04 16:33:46 +0530

    Project initial setup

---
+++ xyz-pharma/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class homepage extends React.Component{
+
+
+  render(){
+
+    return(
+    
+      <React.Fragment>
+        <h1>hi there!</h1>
+      </React.Fragment>
+    )
+  }
+
+  
+}
+
+export default homepage;
